0d16df03277b73091858df4dd066a19bb7a41471
DAghaSolent <5aghad30@solent.ac.uk>
2025-05-28 11:49:38 +0100

    Implemented ViewModel to hold text entries and function to add text entries

---
+++ app/src/main/java/com/example/codingassessment/TextViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.codingassessment
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+
+data class TextEntry(
+    val message: String,
+    val timestamp : String
+)
+
+class TextViewModel : ViewModel(){
+    val textEntries = mutableListOf<TextEntry>()
+    val textEntriesLive = MutableLiveData<MutableList<TextEntry>>()
+
+    val simpleDateFormat = SimpleDateFormat("HH:mm a", Locale.getDefault())
+
+    fun addTextEntry(message : String){
+        val currentTime = simpleDateFormat.format(Date())
+        /* There are other cleaner options that can be utilised to retrieve the current time in Hour
+           and minute format however they are for higher API level 26 and above, to keep the
+           consistency for this assessment I have decided to go with an option that will apply with
+           the default project API level 24.
+        */
+
+        val textEntry = TextEntry(message, currentTime)
+
+        textEntries.add(textEntry)
+        textEntriesLive.value = textEntries
+    }
+}
