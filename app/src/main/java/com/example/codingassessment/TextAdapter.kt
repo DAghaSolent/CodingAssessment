eca3605b4e4dd1041936327aaae45fd7c60e3d40
DAghaSolent <5aghad30@solent.ac.uk>
2025-05-28 23:48:58 +0100

    Refactored code to implement Data binding in Text Adapter and xml layout file that presents the text entries in the recycler view through the text adapter

--- app/src/main/java/com/example/codingassessment/TextAdapter.kt
+++ app/src/main/java/com/example/codingassessment/TextAdapter.kt
@@ -1,31 +1,26 @@
 package com.example.codingassessment
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.codingassessment.databinding.ItemTextLayoutBinding
 
 class TextAdapter(var msgText: List<String>, var timestampText: List<String>) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    RecyclerView.Adapter<TextAdapter.TextViewHolder>() {
 
-    inner class TextViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textViewMessage = itemView.findViewById(R.id.messageText) as TextView
-        val textViewTimestamp = itemView.findViewById(R.id.timestampText) as TextView
-    }
+    inner class TextViewHolder(val binding: ItemTextLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val inflatedLayout = layoutInflater.inflate(R.layout.item_text_layout, parent, false)
-        return TextViewHolder(inflatedLayout)
+        val binding = ItemTextLayoutBinding.inflate(layoutInflater, parent, false)
+        return TextViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, index: Int) {
-
-        val textViewHolder = holder as TextViewHolder
-
-        textViewHolder.textViewMessage.text = msgText[index]
-        textViewHolder.textViewTimestamp.text = timestampText[index]
+    override fun onBindViewHolder(holder: TextViewHolder, index: Int) {
+        holder.binding.message = msgText[index]
+        holder.binding.timestamp = timestampText[index]
+        holder.binding.executePendingBindings()
     }
 
     override fun getItemCount(): Int {
